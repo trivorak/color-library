9c4f3e851601807e5455c2828a6e145e06892b3b
Drake Champagne <trivorak@gmail.com>
2023-08-14 06:12:38 -0500

    finished palette creator...yay brain

--- scripts/color-bit-palette-generator.py
+++ scripts/color-bit-palette-generator.py
@@ -4,11 +4,21 @@ print("Please Input:")
 redB = int(input("Red bits: "))
 greenB = int(input("Green bits: "))
 blueB = int(input("Blue bits: "))
+paletteName = input("Name of Palette? ")
+
+# Create Palette File
+colorFile = open(paletteName+".gpl","w")
+
+# Create Header in palette File
+colorFile.write("GIMP Palette\n")
+colorFile.write("Name:"+paletteName+"\n")
+colorFile.write("#\n")
+
 
 totalB = redB + greenB + blueB
 
 binRep = str()
-print("Total bits = " + str(totalB))
+#print("Total bits = " + str(totalB))
 
 for x in range(totalB):
     binRep = binRep + str(1)
@@ -17,9 +27,20 @@ for x in range(totalB):
 totalVal = int(binRep,2)+1
 aBinaryList = []
 
-print(binRep)
-print("Max Value = " + str(int(binRep,2)))
-print("Total Values = " + str(int(binRep,2)+1))
+#print(binRep)
+#print("Max Value = " + str(int(binRep,2)))
+#print("Total Values = " + str(int(binRep,2)+1))
+
+def getMaxValue(bits):
+    num = str()
+    for x in range(bits):
+        num = num + str(1)
+    return num
+
+#Get Max Value
+redM = int(getMaxValue(redB),2)
+greenM = int(getMaxValue(greenB),2)
+blueM = int(getMaxValue(blueB),2)
 
 # Create all Binary Values Needed for Color Gen
 for x in range(totalVal):
@@ -32,10 +53,30 @@ for x in range(totalVal):
 
     aBinaryList.append(remv)
     
-#Debug    
-print(aBinaryList)
+##Debug    
+#print(aBinaryList)
+#print(getMaxValue(redB))
+#print(getMaxValue(greenB))
+#print(getMaxValue(blueB))
 
-#     Splitting Actions:
-#     print("Red = " + str(remv[:redB]))
-#     print("Green = " + str(remv[redB:-blueB]))
-#     print("Blue = " + str(remv[-blueB]))
+
+for i in aBinaryList:
+    #Splitting Actions:
+    redS = str(i[:redB])
+    greenS = str(i[redB:-blueB])
+    blueS = str(i[-blueB:])
+    
+    #Convert to desc
+    redS = int(redS,2)
+    greenS = int(greenS,2)
+    blueS = int(blueS,2)
+    
+    redV = round(redS/redM * 255)
+    greenV = round(greenS/greenM * 255)
+    blueV = round(blueS/blueM * 255)
+
+    colorFile.write(str(redV) + " " + str(greenV) + " " + str(blueV) + " " + "Color\n")
+    #Debug
+    print(str(redV) + " " + str(greenV) + " " + str(blueV) + " " + "Color")
+
+colorFile.close()
